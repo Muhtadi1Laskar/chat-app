483cfe80fe066c26ff2be8aec33c8e7eeba64f34
Muhtadi Laskar <laskar90muhtadi@gmail.com>
2018-12-25 12:33:53 +0600

    Format timestamp using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -6,18 +6,19 @@ let socket = io();
        });
 
        socket.on('newMessage', function(newMessage){
-           console.log("Got a new message", newMessage);
+           let formattedTime = moment(newMessage.createdAt).format('h:mm a');
            let li = jQuery('<li></li>');
-           li.text(`${newMessage.from}: ${newMessage.text}`);
+           li.text(`${newMessage.from} ${formattedTime}: ${newMessage.text}`);
 
            jQuery('#messages').append(li);
        });
 
        socket.on('newLocationMessage', function(message){
+           let formattedTime = moment(message.createdAt).format('h:mm a');
            let li = jQuery('<li></li>');
            let a = jQuery('<a target="_blank">My current location</a>');
 
-           li.text(`${message.from}: `);
+           li.text(`${message.from} ${formattedTime}: `);
            a.attr('href', message.url)
            li.append(a);
            jQuery('#messages').append(li);
